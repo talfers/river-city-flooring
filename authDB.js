b27ac51cf2f175b36a940b15d4c5d389bb469083
Taylor Alfers <Taylor>
2019-05-29 00:09:19 -0600

    mlab setup

---
+++ authDB.js
@@ -0,0 +1,6 @@
+const authDB = {
+  user: 'taylor',
+  password: 'PinBack3*0'
+}
+
+module.exports = authDB;
--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const authDB = require('../authDB');
 mongoose.set('debug', true);
-mongoose.connect('mongodb://localhost/rchf', { useNewUrlParser: true });
+mongoose.connect(`mongodb://${authDB.user}:${authDB.password}@ds153400.mlab.com:53400/rivercity-hardwood`, { useNewUrlParser: true });
 
 mongoose.Promise = Promise;
 
