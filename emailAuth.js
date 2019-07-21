2cf599b1683e9403f2dbe57dd97ff4d2c885c192
Taylor Alfers <Taylor>
2019-07-21 09:41:18 -0600

    layer

--- emailAuth.js
+++ emailAuth.js
@@ -1,6 +1,6 @@
 const emailAuth = {
-    user: '8fabf5051c1337495f575a9cf31537b9',
-    pass: '361e26403e142fc09d6a230ba4b83e23'
+    user: process.env.USER,
+    pass: process.env.PASS
 };
 
 module.exports = emailAuth;
